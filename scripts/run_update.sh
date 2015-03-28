48387c236660a1610cbaba24641d20176b185f02
Mattia Rizzolo <mattia@mapreri.org>
2015-03-28 18:39:12 -0400

    fixup the wrapper script of the script to run the actual script if the user is already the correct user

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+set -e
+USER=$(whoami)
+
 if [ "$USER" != "public-udd-mirror" ] ; then
-    echo "The updater script is not run as the public-udd-mirror user."
+    echo "This script has been called by $USER"
+    echo "The updater script is thought to be run by the public-udd-mirror user."
     echo "Trying to sudo to that user..."
-    sudo su -c /var/www/scripts/update_udd.sh public-udd-mirror
+    sudo -u public-udd-mirror /var/www/scripts/update_udd.sh
+else
+    /var/www/scripts/update_udd.sh
 fi
