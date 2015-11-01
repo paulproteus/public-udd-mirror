498943a79f05cda88ba935eaa5b011e266737e68
Mattia Rizzolo <mattia@mapreri.org>
2015-11-01 18:58:49 +0000

    run_update: just be relative when calling the actual update script

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -2,12 +2,13 @@
 
 set -e
 USER=$(whoami)
+DIR=$(dirname "$0")
 
 if [ "$USER" != "public-udd-mirror" ] ; then
     echo "This script has been called by $USER"
     echo "The updater script is thought to be run by the public-udd-mirror user."
     echo "Trying to sudo to that user..."
-    sudo -u public-udd-mirror /var/www/scripts/update_udd.sh
+    sudo -u udd "$DIR/update_udd.sh"
 else
-    /var/www/scripts/update_udd.sh
+    "$DIR/update_udd.sh"
 fi
