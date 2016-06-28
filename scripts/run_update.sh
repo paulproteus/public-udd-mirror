4139f3268d919f9dd7b5de244d7c8a9cae466f73
Mattia Rizzolo <mattia@debian.org>
2016-06-28 08:33:00 +0000

    the user is called 'public-udd-mirror', not 'udd'

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -8,7 +8,7 @@ if [ "$USER" != "public-udd-mirror" ] ; then
     echo "This script has been called by $USER"
     echo "The updater script is thought to be run by the public-udd-mirror user."
     echo "Trying to sudo to that user..."
-    sudo -u udd "$DIR/update_udd.sh"
+    sudo -u public-udd-mirror "$DIR/update_udd.sh"
 else
     "$DIR/update_udd.sh"
 fi
