2e99e95c738e728596ac971e340f05029c98b0b4
Mattia Rizzolo <mattia@debian.org>
2025-07-22 11:50:23 +0200

    postgres 17 seems to be picker of quoting identifiers vs literals
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- scripts/update_udd.sh
+++ scripts/update_udd.sh
@@ -67,13 +67,13 @@ fi
 if [ "$UDD_FILENAME" -nt "$SUCCESS_STAMP" ] ; then
     # Ensure public access login accounts exist
     if ! sudo -u postgres psql -t -c '\du' |grep -qw udd; then
-        echo "CREATE USER 'udd' WITH PASSWORD 'udd'" | sudo -u postgres psql -a
+        echo "CREATE USER \"udd\" WITH PASSWORD 'udd'" | sudo -u postgres psql -a
     fi
     if ! sudo -u postgres psql -t -c '\du' |grep -qw udd-mirror; then
-        echo "CREATE USER 'udd-mirror' WITH PASSWORD 'udd-mirror'" | sudo -u postgres psql -a
+        echo "CREATE USER \"udd-mirror\" WITH PASSWORD 'udd-mirror'" | sudo -u postgres psql -a
     fi
     if ! sudo -u postgres psql -t -c '\du' |grep -qw public-udd-mirror; then
-        echo "CREATE USER 'public-udd-mirror' WITH PASSWORD 'public-udd-mirror'" | sudo -u postgres psql -a
+        echo "CREATE USER \"public-udd-mirror\" WITH PASSWORD 'public-udd-mirror'" | sudo -u postgres psql -a
     fi
     sudo -u postgres psql -a <<- "EOF"
     ALTER DEFAULT PRIVILEGES FOR USER "udd" IN SCHEMA "public" GRANT SELECT ON TABLES TO "udd";
